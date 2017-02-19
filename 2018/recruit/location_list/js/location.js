469f5c0efd4ee276efec9108a4b16296aa0b5f17
7imac16 <sato@seven-inc.co.jp>
2017-02-20 00:20:39 +0900

    所在地一覧｜js修正

--- 2018/recruit/location_list/js/location.js
+++ 2018/recruit/location_list/js/location.js
@@ -4,135 +4,142 @@ $(function () {
 		timer = null,
 		href = location.href,
 		param = href.split('?');
-
-	locationSetter( param );
-	
 	/**
 	* PC幅のみ
 	*/
-	$window.on( 'load resize', function(){
+	$window.on( 'load', function(){
 
 		clearTimeout( timer );
 
 		timer = setTimeout( function(){
-
 			var $windowWidth = $window.innerWidth();
 
 			if(  breakpoint <= $windowWidth ){
 
-					/**
-					* リサイズ時のUI挙動リセット
-					*/
-
-					$('.area_data').add($('.selectArea_area')).add($('.activeMap')).each(function(){
-						$(this).removeAttr('style')
-					});
-
-				(function setLayout(){
-					$('.area_data').each(function(){
-						var $this = $(this);
-						var $myWrap = $this.closest('.area_wrap');
-						var $myBtn = $myWrap.find('.area_btn');
-						var valLeft = (function(){
-							if($myWrap.attr('data-area') == 'kyushu'){
-								return -(($this.outerWidth() - $myBtn.outerWidth())/5);
-							}
-							else {
-								return -(($this.outerWidth() - $myBtn.outerWidth())/2);
-							}
-						}());
-						var valTop = (function(){
-							if($myBtn.hasClass('arrow_bottom')){
-								return 44;
-							}
-							else {
-								return -10 - $this.outerHeight();
-							}
-						}());
-						$this.css({
-							'top': valTop,
-							'left': valLeft,
-							'display': 'none'
-						});
-					});
-					$('.activeMap').hide();
-					if( ! param[1] ) $('.selectArea').hide().eq(0).show();
-				}());
-
-
-				/**
-				* コースを選ぶ（上部メニュー）ボタンをクリックしたとき
-				*/
-			    $('.type_btn').click(function(){
-					var $this = $(this);
-					var speed = 400;
-					$('.selectArea').each(function(){
-					    var _$this = $(this);
-					    if(_$this.find('.area_btn.active')){
-							_$this.find('.area_btn.active').click();
-							changeActiveMap(_$this.find('.activeMap'), 'base');
-							_$this.stop(true,true).find('.activeMap').hide();
-						}
-						_$this.hide();
-						if(_$this.hasClass($this.attr('data-type'))){
-							_$this.stop(true,true).fadeIn(speed);
-						}
-			        });
-			    });
-
-				/**
-				* 地図のボタンをクリックしたとき
-				*/
-			    $('.area_btn, .area_data_closer').click(function( e ){
-			    	e.preventDefault();
-			    	e.stopPropagation();
-			        var $this = $(this).hasClass('area_data_closer') ? $(this).closest('.selectArea_area').find('.area_btn') : $(this);
-			        // var $this = $(this).hasClass('area_data_closer') ? $(this).closest('.selectArea_area').find('.area_btn') : $(this);
-
-
-					var $myWrap = $this.closest('.area_wrap');
-					var $map = $this.closest('.selectArea').find('.activeMap');
-			        var speed = 300;
-			        if($this.hasClass('active')){
-						$this.removeClass('active').css({'background-image':'url("img/area_btn_search.svg")'});
-						$myWrap.find('.area_data').stop(true,true).fadeOut(speed);
-						$map.fadeOut(speed);
-			        }
-			        else {
-						$('.area_btn').removeClass('active').css({'background-image':'url("img/area_btn_search.svg")'});
-						$this.addClass('active').css({'background-image':'url("img/area_btn_close.svg")'});
-						changeActiveMap($map, $myWrap.attr('data-area'));
-						$map.stop(true,true).fadeIn(speed);
-						$('.area_data').fadeOut(speed);
-						$myWrap.find('.area_data').stop(true,true).fadeIn(speed);
-			        }
-			    });
-
+				locationSetter( param, 'pc' );				
+				pc( $window );
 
 			} else {
 
-					$('.area_data').add($('.selectArea')).each(function(){
-						$(this).removeAttr('style')
-					});
-					$('.selectArea_area').each(function(){
-						$(this).hide();
-					});
-					$('.activeMap').hide();
-					
+				sp()
+				locationSetter( param, 'sp' );//spの場合はあとに書く
 
-					$('.selectArea_ttl_wrap').on( 'click', function( e ){
-						e.stopPropagation();
-						e.preventDefault();
-						if( $(this).hasClass('active') ){
-							$(this).parent().find('.selectArea_area').slideUp(1000).end().end().removeClass('active').css({'background-image':'url("img/area_btn_search.svg")'});
-						} else {
-							$(this).parent().find('.selectArea_area').slideDown(1000).end().end().addClass('active').css({'background-image':'url("img/area_btn_close.svg")'});
-						}
-
-					});
 			}
 		});
 	} );
+
+	function pc( $window ){
+		
+		/**
+		* リサイズ時のUI挙動リセット
+		*/
+		$('.area_data').add($('.selectArea_area')).add($('.activeMap')).each(function(){
+			$(this).removeAttr('style')
+		});
+
+		(function setLayout(){
+			$('.area_data').each(function(){
+				var $this = $(this);
+				var $myWrap = $this.closest('.area_wrap');
+				var $myBtn = $myWrap.find('.area_btn');
+				var valLeft = (function(){
+					if($myWrap.attr('data-area') == 'kyushu'){
+						return -(($this.outerWidth() - $myBtn.outerWidth())/5);
+					}
+					else {
+						return -(($this.outerWidth() - $myBtn.outerWidth())/2);
+					}
+				}());
+				var valTop = (function(){
+					if($myBtn.hasClass('arrow_bottom')){
+						return 44;
+					}
+					else {
+						return -10 - $this.outerHeight();
+					}
+				}());
+				$this.css({
+					'top': valTop,
+					'left': valLeft,
+					'display': 'none'
+				});
+			});
+			$('.activeMap').hide();
+			if( ! param[1] ) $('.selectArea').hide().eq(0).show();
+		}());
+
+
+		/**
+		* コースを選ぶ（上部メニュー）ボタンをクリックしたとき
+		*/
+	    $('.type_btn').click(function(){
+			var $this = $(this);
+			var speed = 400;
+			$('.selectArea').each(function(){
+			    var _$this = $(this);
+			    if(_$this.find('.area_btn.active')){
+					_$this.find('.area_btn.active').click();
+					changeActiveMap(_$this.find('.activeMap'), 'base');
+					_$this.stop(true,true).find('.activeMap').hide();
+				}
+				_$this.hide();
+				if(_$this.hasClass($this.attr('data-type'))){
+					_$this.stop(true,true).fadeIn(speed);
+				}
+	        });
+	    });
+
+		/**
+		* 地図のボタンをクリックしたとき
+		*/
+	    $('.area_btn, .area_data_closer').click(function( e ){
+	    	e.preventDefault();
+	    	e.stopPropagation();
+	        var $this = $(this).hasClass('area_data_closer') ? $(this).closest('.selectArea_area').find('.area_btn') : $(this);
+	        // var $this = $(this).hasClass('area_data_closer') ? $(this).closest('.selectArea_area').find('.area_btn') : $(this);
+
+
+			var $myWrap = $this.closest('.area_wrap');
+			var $map = $this.closest('.selectArea').find('.activeMap');
+	        var speed = 300;
+	        if($this.hasClass('active')){
+				$this.removeClass('active').css({'background-image':'url("img/area_btn_search.svg")'});
+				$myWrap.find('.area_data').stop(true,true).fadeOut(speed);
+				$map.fadeOut(speed);
+	        }
+	        else {
+				$('.area_btn').removeClass('active').css({'background-image':'url("img/area_btn_search.svg")'});
+				$this.addClass('active').css({'background-image':'url("img/area_btn_close.svg")'});
+				changeActiveMap($map, $myWrap.attr('data-area'));
+				$map.stop(true,true).fadeIn(speed);
+				$('.area_data').fadeOut(speed);
+				$myWrap.find('.area_data').stop(true,true).fadeIn(speed);
+	        }
+	    });
+
+	}
+
+	function sp(){
+		$('.area_data').add($('.selectArea')).each(function(){
+			$(this).removeAttr('style')
+		});
+		$('.selectArea_area').each(function(){
+			$(this).hide();
+		});
+		$('.activeMap').hide();
+		
+
+		$('.selectArea_ttl_wrap').on( 'click', function( e ){
+			e.stopPropagation();
+			e.preventDefault();
+			if( $(this).hasClass('active') ){
+				$(this).parent().find('.selectArea_area').slideUp(1000).end().end().removeClass('active').css({'background-image':'url("img/area_btn_search.svg")'});
+			} else {
+				$(this).parent().find('.selectArea_area').slideDown(1000).end().end().addClass('active').css({'background-image':'url("img/area_btn_close.svg")'});
+			}
+
+		});		
+	}
 
 	function changeActiveMap(map, data){
 		var src = $(map).attr('src');
@@ -142,11 +149,12 @@ $(function () {
 	}
 
 	/**
-	* 別ページから　?〜パラムを送信して初期表示コンテンツを指定可能
-	* example ../location_list/index.html?chiiki_a
+	* 別ページから　?〜パラムを送信して初期表示コンテンツを指定する。
+	* example ahref="../location_list/index.html?chiiki_a"
 	* @param param { str } <div class="selectArea ~ "> に指定してあるクラス
+	* @param device { str } sp, pc 挙動を変える。
 	*/
-	function locationSetter( param ){
+	function locationSetter( param, device ){
 		// var href = location.href,
 		// 	param = href.split('?');
 
@@ -157,11 +165,28 @@ $(function () {
 
 		console.log( $targetSection )
 
-		$.each( $('.selectArea'), function(){
-			$(this).hide();
-		});		
+		if( device == "pc" ){
+			$.each( $('.selectArea'), function(){
+				$(this).hide();
+			});		
 
-		$targetSection.fadeIn('fast');
+			$targetSection.fadeIn('fast');			
+		} else {
+			$.each( $('.selectArea'), function(){
+				var sectionClass = $(this).attr('class'),
+					reg = new RegExp( ".*" + param[1] );
+
+				if( reg.test( sectionClass ) ){
+					var $openTarget = $(this).find('.selectArea_area');
+					$openTarget.fadeIn('fast', function(){
+						$('body,html').animate({
+							scrollTop: $openTarget.offset().top
+						}, 0);
+					});
+				}
+			})
+		}
+
 	}
 	
 });
